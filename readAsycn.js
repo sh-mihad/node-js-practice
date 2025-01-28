bbcb3f86fd31b1edf5497ad6e02bcb160d371d2b
Sabbir <sh.mihad.cse@gmail.com>
2025-01-28 13:20:48 +0600

    read fs asynchrounsly

---
+++ readAsycn.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+ fs.readFile("data.text","utf-8",(err,data)=>{
+    if(err){
+        console.log("there was an error");
+    }else{
+        console.log(data);
+    }
+})
